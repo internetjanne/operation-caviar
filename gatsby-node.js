2e33b3317eed9880c13734bb16ec5ba5b804a028
Viktor Schultzberg <viktor.schultzberg@isotop.se>
2023-09-28 09:15:19 +0200

    hubba

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require('fs');
 
 exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
 
@@ -31,4 +32,22 @@ exports.createPages = ({ actions }) => {
     toPath: '/',
     isPermanent: true,
   })
+}
+
+exports.onPreInit = () => {
+  if (process.argv[2] === "build") {
+    fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
+    fs.renameSync(
+      path.join(__dirname, "public"),
+      path.join(__dirname, "public_dev")
+    )
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
+  fs.renameSync(
+    path.join(__dirname, "public_dev"),
+    path.join(__dirname, "public")
+  )
 }
